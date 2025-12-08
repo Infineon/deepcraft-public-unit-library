d8c9ab1175db8ed31735a16d65b0e1c21d71e884
Gustav Nilsson <61567735+Hoozt@users.noreply.github.com>
2025-12-08 14:52:20 +0100

    Sync with studio (#5)
    * Synced with current studio unit library
    
    * Sync with studio:
    - Fixed typo in Quantize
    - Int8 sub support
    - Yolo8/11 support BB filter
    - Int8 resizing support

--- Imaginet.Units.Math/Multifold/Sub/sub.h
+++ Imaginet.Units.Math/Multifold/Sub/sub.h
@@ -20,3 +20,49 @@ static inline void sub_f32(
 	}
 }
 #pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "sub_i8"
+static inline void sub_i8(
+	const int8_t* restrict a,
+	const int8_t* restrict b,
+	int l, int g1, int m, int g2, int r,
+	int8_t* restrict output)
+{
+	int index = 0;
+	for (int x = 0; x < l; x++) {
+		for (int i = 0; i < g1; i++) {
+			for (int y = 0; y < m; y++) {
+				for (int j = 0; j < g2; j++) {
+					for (int z = 0; z < r; z++) {
+						output[index] = a[index] - b[x * m * r + y * r + z];
+						index++;
+					}
+				}
+			}
+		}
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "sub_i32"
+static inline void sub_i32(
+	const int32_t* restrict a,
+	const int32_t* restrict b,
+	int l, int g1, int m, int g2, int r,
+	int32_t* restrict output)
+{
+	int index = 0;
+	for (int x = 0; x < l; x++) {
+		for (int i = 0; i < g1; i++) {
+			for (int y = 0; y < m; y++) {
+				for (int j = 0; j < g2; j++) {
+					for (int z = 0; z < r; z++) {
+						output[index] = a[index] - b[x * m * r + y * r + z];
+						index++;
+					}
+				}
+			}
+		}
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
