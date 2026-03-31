144422974077ae880bb2b928bfb78b772aa6729f
Gustav Nilsson <jobb.gustavnilsson@gmail.com>
2026-03-31 14:35:00 +0200

    Nearest done, first iteration. Have not tested yet.

--- Imaginet.Units.Signal/TemporalAnalysis/Resample/nearest.h
+++ Imaginet.Units.Signal/TemporalAnalysis/Resample/nearest.h
@@ -4,7 +4,8 @@
 
 #pragma IMAGINET_FRAGMENT_BEGIN "data_struct_f32"
 typedef struct {       
-    int initialized;       // To handle first inference
+    int initialized;          // To handle first inference
+	int current_inference;    // Modulo cycle
 } nearest_state_f32;
 #pragma IMAGINET_FRAGMENT_END
 
@@ -16,6 +17,7 @@ static inline void nearest_init_f32(int count, float* state_ptr)
 {
 	nearest_state_f32* state = (nearest_state_f32*)state_ptr;
 	state->initialized = 0;
+	state->current_inference = 0;
 }
 
 #pragma IMAGINET_FRAGMENT_END
@@ -26,15 +28,22 @@ static inline void nearest_init_f32(int count, float* state_ptr)
 
 static inline void nearest_f32(const float* restrict input, float* restrict output, int count, float* state_ptr, float output_samples_per_input)
 {
-	nearest_state_f32 state = (nearest_state_f32*)state_ptr;
-	float* previous_value = (float*)((char*)handle + sizeof(nearest_state_f32));
+	nearest_state_f32* state = (nearest_state_f32*)state_ptr;
+	float* previous_value = (float*)((char*)state_ptr + sizeof(nearest_state_f32));
+
+	int use_previous = state->initialized && (state->current_inference < output_samples_per_input / 2.0f);
 
 	for (int i = 0; i < count; ++i)
-		
-		*(output++) = *(input++);
+		output[i] = use_previous ? previous_value[i] : input[i];
 
-
-	
+	state->current_inference++;
+	if (state->current_inference >= (int)output_samples_per_input)
+	{
+		for (int i = 0; i < count; ++i)
+			previous_value[i] = input[i];
+		state->current_inference = 0;
+		state->initialized = 1;
+	}
 }
 
 #pragma IMAGINET_FRAGMENT_END
