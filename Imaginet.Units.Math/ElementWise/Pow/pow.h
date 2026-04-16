e48553bd9934a41becd16e745a698858ef2d6f40
Gustav Nilsson <jobb.gustavnilsson@gmail.com>
2026-04-16 15:29:04 +0200

    Added int8, int16 and int32 implementations of pow and subfromconstant

--- Imaginet.Units.Math/ElementWise/Pow/pow.h
+++ Imaginet.Units.Math/ElementWise/Pow/pow.h
@@ -11,3 +11,39 @@ static inline void pow_f32(const float* restrict x, int count, float exponent, f
 }
 #pragma IMAGINET_FRAGMENT_END
 
+#pragma IMAGINET_FRAGMENT_BEGIN "pow_i8"
+static inline void pow_i8(const int8_t* restrict x, int count, float exponent, int8_t* restrict result)
+{
+	for (int i = 0; i < count; i++) {
+		float val = powf((float)*x++, exponent);
+		if (val > 127.0f) val = 127.0f;
+		else if (val < -128.0f) val = -128.0f;
+		*result++ = (int8_t)val;
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "pow_i16"
+static inline void pow_i16(const int16_t* restrict x, int count, float exponent, int16_t* restrict result)
+{
+	for (int i = 0; i < count; i++) {
+		float val = powf((float)*x++, exponent);
+		if (val > 32767.0f) val = 32767.0f;
+		else if (val < -32768.0f) val = -32768.0f;
+		*result++ = (int16_t)val;
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "pow_i32"
+static inline void pow_i32(const int32_t* restrict x, int count, float exponent, int32_t* restrict result)
+{
+	for (int i = 0; i < count; i++) {
+		float val = powf((float)*x++, exponent);
+		if (val > 2147483647.0f) val = 2147483647.0f;
+		else if (val < -2147483648.0f) val = -2147483648.0f;
+		*result++ = (int32_t)val;
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
--- Imaginet.Units.Math/ElementWise/SubFromConstant/subir.h
+++ Imaginet.Units.Math/ElementWise/SubFromConstant/subir.h
@@ -10,3 +10,42 @@ static inline void subir_f32(
 	}
 }
 #pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "subir_i8"
+static inline void subir_i8(
+	const int8_t* restrict x,
+	int count,
+	int8_t immediate,
+	int8_t* restrict output)
+{
+	for (int i = 0; i < count; i++) {
+		output[i] = immediate - x[i];
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "subir_i16"
+static inline void subir_i16(
+	const int16_t* restrict x,
+	int count,
+	int16_t immediate,
+	int16_t* restrict output)
+{
+	for (int i = 0; i < count; i++) {
+		output[i] = immediate - x[i];
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "subir_i32"
+static inline void subir_i32(
+	const int32_t* restrict x,
+	int count,
+	int32_t immediate,
+	int32_t* restrict output)
+{
+	for (int i = 0; i < count; i++) {
+		output[i] = immediate - x[i];
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
