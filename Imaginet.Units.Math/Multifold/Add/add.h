35b72c9530ede9457568cc1dba8a87e99927fde7
Gustav Nilsson <jobb.gustavnilsson@gmail.com>
2026-04-17 13:04:24 +0200

    Int8 implementation added to multi-tensor add

--- Imaginet.Units.Math/Multifold/Add/add.h
+++ Imaginet.Units.Math/Multifold/Add/add.h
@@ -1,9 +1,82 @@
+#pragma IMAGINET_INCLUDES_BEGIN
+#include <stdint.h>
+#pragma IMAGINET_INCLUDES_END
+
 #pragma IMAGINET_FRAGMENT_BEGIN "add_f32"
 static inline void add_f32(
 	const float* restrict a,
 	const float* restrict b,
 	int l, int g1, int m, int g2, int r,
 	float* restrict output)
+{
+	int index = 0;
+	for (int x = 0; x < l; x++) {
+		for (int i = 0; i < g1; i++) {
+			for (int y = 0; y < m; y++) {
+				for (int j = 0; j < g2; j++) {
+					for (int z = 0; z < r; z++) {
+						output[index] = a[index] + b[x * m * r + y * r + z];
+						index++;
+					}
+				}
+			}
+		}
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "add_i8"
+static inline void add_i8(
+	const int8_t* restrict a,
+	const int8_t* restrict b,
+	int l, int g1, int m, int g2, int r,
+	int8_t* restrict output)
+{
+	int index = 0;
+	for (int x = 0; x < l; x++) {
+		for (int i = 0; i < g1; i++) {
+			for (int y = 0; y < m; y++) {
+				for (int j = 0; j < g2; j++) {
+					for (int z = 0; z < r; z++) {
+						output[index] = a[index] + b[x * m * r + y * r + z];
+						index++;
+					}
+				}
+			}
+		}
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "add_i16"
+static inline void add_i16(
+	const int16_t* restrict a,
+	const int16_t* restrict b,
+	int l, int g1, int m, int g2, int r,
+	int16_t* restrict output)
+{
+	int index = 0;
+	for (int x = 0; x < l; x++) {
+		for (int i = 0; i < g1; i++) {
+			for (int y = 0; y < m; y++) {
+				for (int j = 0; j < g2; j++) {
+					for (int z = 0; z < r; z++) {
+						output[index] = a[index] + b[x * m * r + y * r + z];
+						index++;
+					}
+				}
+			}
+		}
+	}
+}
+#pragma IMAGINET_FRAGMENT_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "add_i32"
+static inline void add_i32(
+	const int32_t* restrict a,
+	const int32_t* restrict b,
+	int l, int g1, int m, int g2, int r,
+	int32_t* restrict output)
 {
 	int index = 0;
 	for (int x = 0; x < l; x++) {
