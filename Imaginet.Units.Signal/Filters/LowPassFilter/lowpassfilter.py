a3f7a437a4b3f0d2b52583564e9c1b4167ac771f
Ben Treeby <benedicttreeby@hotmail.com>
2026-06-15 11:39:22 +0200

    Adding python implementations of lowpass, highpass and notch filters (#50)
    * Added python implementations of highpass, lowpass and notch filters
    
    * Fixed #pragma in python files
    
    ---------
    
    Co-authored-by: Treeby Benedict (CSS IMAG ML) <Benedict.Treeby@infineon.com>

---
+++ Imaginet.Units.Signal/Filters/LowPassFilter/lowpassfilter.py
@@ -0,0 +1,14 @@
+#pragma IMAGINET_INCLUDES_BEGIN
+import numpy as np
+from scipy import signal
+# pragma IMAGINET_INCLUDES_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "lowpass_filter"
+
+
+def lowpass_filter(input, output, cutoff_freq, freq, order=1):
+    b, a = signal.butter(order, cutoff_freq, "lowpass", fs=freq)
+    result = signal.filtfilt(b, a, input, axis=0)
+    np.copyto(output, result)
+
+#pragma IMAGINET_FRAGMENT_END
---
+++ Imaginet.Units.Signal/Filters/Notch/notch.py
@@ -0,0 +1,15 @@
+#pragma IMAGINET_INCLUDES_BEGIN
+import numpy as np
+from scipy import signal
+#pragma IMAGINET_INCLUDES_END
+
+#pragma IMAGINET_FRAGMENT_BEGIN "notch_filter"
+
+
+def notch_filter(input, output, freq, frequency, Q):
+    b, a = signal.iirnotch(frequency, Q, freq)
+    result = signal.filtfilt(b, a, input, axis=0)
+    np.copyto(output, result)
+
+
+#pragma IMAGINET_FRAGMENT_END
